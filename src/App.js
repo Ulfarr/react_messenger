3bae1d0ba0bbaf55ac7804137eb0e6b402abdb4b
Adam Moran <pipboy.moran9@gmail.com>
2019-03-19 13:44:03 -0500

    2nd checkpoint commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as firebase from 'firebase';
+
+import RoomList from './components/RoomList';
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUUon38Q3ticyydIaHuaI4XeHmMeW3vwc",
+    authDomain: "messaging-app-89908.firebaseapp.com",
+    databaseURL: "https://messaging-app-89908.firebaseio.com",
+    projectId: "messaging-app-89908",
+    storageBucket: "messaging-app-89908.appspot.com",
+    messagingSenderId: "382519844522"
+  };
+  firebase.initializeApp(config);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRoom: null,
+      user: null
+    };
+  }
+
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        < RoomList firebase={firebase} />
+      </div>
+    );
+  }
+}
+
+export default App;
